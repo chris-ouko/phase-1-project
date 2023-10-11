0957ad1d91d592ef6a89936f26d8ea1953275643
+ <+>
2023-10-11 15:18:45 +0300

    Updated the repo to public

--- index.js
+++ index.js
@@ -1,64 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const catImageContainer = document.getElementById("cat-image-container");
-    const randomCatButton = document.getElementById("random-cat-button");
-    const catTagInput = document.getElementById("cat-tag");
-    const catTextInput = document.getElementById("cat-text");
-    const catSizeSelect = document.getElementById("cat-size");
-    const catColorInput = document.getElementById("cat-color");
-    const catFilterSelect = document.getElementById("cat-filter");
-    const catWidthInput = document.getElementById("cat-width");
-    const catHeightInput = document.getElementById("cat-height");
-    const catHtmlCheckbox = document.getElementById("cat-html");
-    const catJsonCheckbox = document.getElementById("cat-json");
-
-    randomCatButton.addEventListener("click", function () {
-        // Build the API URL based on user customization options
-        const baseUrl = "https://cataas.com/cat";
-        let apiUrl = baseUrl;
-
-        if (catTagInput.value) {
-            apiUrl += `/${catTagInput.value}`;
-        }
-
-        if (catTextInput.value) {
-            apiUrl += `/says/${catTextInput.value}`;
-        }
-
-        const selectedSize = catSizeSelect.value;
-        if (selectedSize !== "original") {
-            apiUrl += `?type=${selectedSize}`;
-        }
-
-        const selectedFilter = catFilterSelect.value;
-        if (selectedFilter !== "none") {
-            apiUrl += `?filter=${selectedFilter}`;
-        }
-
-        if (catWidthInput.value) {
-            apiUrl += `?width=${catWidthInput.value}`;
-        }
-
-        if (catHeightInput.value) {
-            apiUrl += `?height=${catHeightInput.value}`;
-        }
-
-        if (catHtmlCheckbox.checked) {
-            apiUrl += "?html=true";
-        }
-
-        if (catJsonCheckbox.checked) {
-            apiUrl += "?json=true";
-        }
-
-        // Fetch a random cat image from the API
-        fetch(apiUrl)
-            .then((response) => response.text())
-            .then((data) => {
-                // Display the cat image in the container
-                catImageContainer.innerHTML = data;
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("https://cataas.com/cat?json=true")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            let imageDiv = document.createElement("div")
+            imageDiv.className = "images"
+            imageDiv.innerHTML = `
+         <img src="https://cataas.com/${data.url}">
+        `
+            document.querySelector(".image1").appendChild(imageDiv)
+            document.querySelector("#random").addEventListener("click", () => {
+                changeButton();
             })
-            .catch((error) => {
-                console.error("Error fetching cat image:", error);
-            });
-    });
-});
+        })
+    function changeButton() {
+        fetch("https://cataas.com/cat?json=true")
+            .then(res => res.json())
+            .then(element => {
+                document.querySelector(".image1").innerHTML = ``
+                let imageDiv2 = document.createElement("div")
+                imageDiv2.className = replacer
+                imageDiv2.innerHTML = `
+        <img src="https://cataas.com/${element.url}">
+        `
+                document.querySelector(".image1").appendChild(imageDiv2)
+        })
+    
+}});
