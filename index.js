99c5bc65528009babeb5920a97d5849422a321df
+ <+>
2023-10-12 15:53:07 +0300

    Introduced the event listener buttons for the like and coment function in the js code

--- index.js
+++ index.js
@@ -34,6 +34,40 @@ function fetchCatImage(criteria) {
             catImage.src = catImageUrl;
     }
 
+// Function to fetch and display a cat image based on criteria
+function fetchCatImage(criteria) {
+    // ... your existing code ...
+
+    // After you set the catImage.src, add event listeners for like and comment.
+    const catImage = document.getElementById('cat-image');
+    const likeButton = document.querySelector('.like-button');
+    const commentButton = document.querySelector('.comment-button');
+    const commentInput = document.querySelector('.comment-input');
+    const commentSubmitButton = document.querySelector('.comment-submit-button');
+    const commentsContainer = document.querySelector('.comments');
+
+    // Simulated data to track likes and comments
+    let likes = 0;
+    const comments = [];
+
+    // Update the like count and display comments
+    likeButton.addEventListener('click', () => {
+        likes += 1;
+        likeButton.textContent = `Like (${likes})`;
+    });
+
+    // Add a new comment to the comments array and display it
+    commentSubmitButton.addEventListener('click', () => {
+        const newComment = commentInput.value;
+        comments.push(newComment);
+
+        // Display all comments
+        const commentsHtml = comments.map(comment => `<p>${comment}</p>`).join('');
+        commentsContainer.innerHTML = commentsHtml;
+        commentInput.value = ''; // Clear the input field
+    });
+}
+
 
 // Function to add an event listener to the "Search" button
 function addSearchButtonListener() {
